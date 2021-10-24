1d423ae8d93b1386f1ae8c0e2780d4d00840a385
BCampos <bccampos105@gmail.com>
2021-10-24 20:32:12 -0300

    Classes PRONTAS

---
+++ Carro.java
@@ -0,0 +1,18 @@
+public class Carro{
+
+
+   //Atributos//
+   private String nome;
+   private int ano;
+   private double valor;
+   private String cor;
+   private boolean isNovo;
+
+   //Construtor   
+   public Carro(String nome, int ano){
+      this.nome = nome;
+      this.ano = ano;
+   }
+
+
+}
---
+++ Vendedor.java
@@ -0,0 +1,11 @@
+public class Vendedor{
+  //Atributos//
+  private String nome;
+  private int re;
+  private String email;
+  private String senha;
+  private boolean isAtivo;
+
+
+
+}
